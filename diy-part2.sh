2d0d3fd49d3d1d81d0b287b86c343d41df68ab26
xiaoyu <113708957+liang5354@users.noreply.github.com>
2023-08-23 13:44:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 ##### Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 ##### Modify hostname
 sed -i 's/OpenWrt/K2P-32M/g' package/base-files/files/bin/config_generate
 ##### Modify the version number
